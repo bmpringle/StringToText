5bb6b4514c560f62a229551c52813408cdd83258
Ben Pringle <benjamin.m.pringle@gmail.com>
2023-01-27 14:25:51 +0000

    Update libpng and compileDeps

--- compileDeps.py
+++ compileDeps.py
@@ -19,22 +19,24 @@ if PICON == True:
 
 output, error = execCmd('git submodule update --init --recursive')
 output, error = execCmd('mkdir -p lib')
-output, error = execCmd('cmake -B build -DCMAKE_BUILD_TYPE=Release -D FT_DISABLE_HARFBUZZ=TRUE -D{}'.format(PICSTRING), './freetype')
+output, error = execCmd('cmake -B build -DCMAKE_OSX_ARCHITECTURES=arm64 -DCMAKE_BUILD_TYPE=Release -D FT_DISABLE_HARFBUZZ=TRUE -D{}'.format(PICSTRING), './freetype')
 output, error = execCmd('make -j8', './freetype/build')
 output, error = execCmd('mv ./libfreetype.a ../../lib/libfreetype.a', './freetype/build')
 
-output, error = execCmd('cmake . -DPNG_ARM_NEON=off -D{}'.format(PICSTRING), './libpng')
+output, error = execCmd('cmake . -DCMAKE_OSX_ARCHITECTURES=arm64 -D{}'.format(PICSTRING), './libpng')
 output, error = execCmd('make -j8', './libpng')
 output, error = execCmd('mv ./libpng16.a ../lib/libpng16.a', './libpng')
 
-output, error = execCmd('cmake . -D{}'.format(PICSTRING), './zlib')
+output, error = execCmd('cmake . -DCMAKE_OSX_ARCHITECTURES=arm64 -D{}'.format(PICSTRING), './zlib')
 output, error = execCmd('make -j8', './zlib')
 output, error = execCmd('mv ./libz.a ../lib/libz.a', './zlib')
+
+output, error = execCmd('echo ${CC}', './zlib')
 
 output, error = execCmd('make -j8 {}'.format('CFLAGS=\"-Wall -Winline -O2 -g -D_FILE_OFFSET_BITS=64 -fPIC\"' if PICON else ''), './bzip2')
 output, error = execCmd('mv ./libbz2.a ../lib/libbz2.a', './bzip2')
 
-output, error = execCmd('cmake . -D{}'.format(PICSTRING), './brotli')
+output, error = execCmd('cmake . -DCMAKE_OSX_ARCHITECTURES=arm64 -D{}'.format(PICSTRING), './brotli')
 output, error = execCmd('make -j8', './brotli')
 output, error = execCmd('mv libbrotlicommon-static.a ../lib', './brotli')
 output, error = execCmd('mv libbrotlidec-static.a ../lib', './brotli')
